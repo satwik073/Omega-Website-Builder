f04a4e37a8c66f5efea85416e3dc4956322189ba
satwikWiz <satwik@wizcommerce.com>
2024-12-28 00:12:48 +0530

    ESLint - wip ( Estest )

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -8,37 +8,41 @@ import React from 'react'
 const Page = async ({
   searchParams,
 }: {
-  searchParams: { plan: Plan; state: string; code: string }
+  searchParams: Promise<{ plan: Plan; state: string; code: string }>
 }) => {
+  const resolvedSearchParams = await searchParams
   const agencyId = await verifyAndAcceptInvitation()
   console.log(agencyId)
 
-  //get the users details
+  // Get the user's details
   const user = await getAuthUserDetails()
   if (agencyId) {
     if (user?.role === 'SUBACCOUNT_GUEST' || user?.role === 'SUBACCOUNT_USER') {
       return redirect('/subaccount')
     } else if (user?.role === 'AGENCY_OWNER' || user?.role === 'AGENCY_ADMIN') {
-      if (searchParams.plan) {
-        return redirect(`/agency/${agencyId}/billing?plan=${searchParams.plan}`)
+      if (resolvedSearchParams.plan) {
+        return redirect(
+          `/agency/${agencyId}/billing?plan=${resolvedSearchParams.plan}`
+        )
       }
-      if (searchParams.state) {
-        const statePath = searchParams.state.split('___')[0]
-        const stateAgencyId = searchParams.state.split('___')[1]
+      if (resolvedSearchParams.state) {
+        const statePath = resolvedSearchParams.state.split('___')[0]
+        const stateAgencyId = resolvedSearchParams.state.split('___')[1]
         if (!stateAgencyId) return <div>Not authorized</div>
         return redirect(
-          `/agency/${stateAgencyId}/${statePath}?code=${searchParams.code}`
+          `/agency/${stateAgencyId}/${statePath}?code=${resolvedSearchParams.code}`
         )
       } else return redirect(`/agency/${agencyId}`)
     } else {
       return <div>Not authorized</div>
     }
   }
+
   const authUser = await currentUser()
   return (
     <div className="flex justify-center items-center mt-4">
       <div className="max-w-[850px] border-[1px] p-4 rounded-xl">
-        <h1 className="text-4xl"> Create An Agency</h1>
+        <h1 className="text-4xl">Create An Agency</h1>
         <AgencyDetails
           data={{ companyEmail: authUser?.emailAddresses[0].emailAddress }}
         />
